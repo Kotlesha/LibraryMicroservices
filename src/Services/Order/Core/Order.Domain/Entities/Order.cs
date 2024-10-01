b480778c1b4910880ef403b08057e7c3def917ab
Alesya <ananko_aa_21@mf.grsu.by>
2024-10-01 15:57:45 +0300

    Изменена реализация свойства TotalCost класса Order, изменён конструктор класса Order

--- src/Services/Order/Core/Order.Domain/Entities/Order.cs
+++ src/Services/Order/Core/Order.Domain/Entities/Order.cs
@@ -5,28 +5,25 @@ namespace Order.Domain.Entities;
 public sealed class Order : AggregateRoot
 {
     public Guid UserId { get; private set; }
-    public decimal TotalCost { get; private set; }
     public DateTimeOffset CreatedDate { get; private set; }
+    public decimal TotalCost => _books.Sum(b => b.Price);
 
     private readonly List<Book> _books = [];
     public IReadOnlyList<Book> Books => _books.AsReadOnly();
 
-    private Order(Guid Id, Guid userId, decimal totalCost, DateTimeOffset createdDate) : base(Id)
+    private Order(Guid Id, Guid userId) : base(Id)
     {
         UserId = userId;
-        TotalCost = totalCost;
-        CreatedDate = createdDate;
+        CreatedDate = DateTimeOffset.UtcNow;
     }
 
-    public static Order Create(Guid userId, decimal totalCost)
+    public static Order Create(Guid userId)
     {
-        var order = new Order(Guid.NewGuid(), userId, totalCost, DateTimeOffset.UtcNow);
+        var order = new Order(Guid.NewGuid(), userId);
         order.Validate();
 
         return order;
     }
-
-    public void Update(Order order) => TotalCost = order.TotalCost;
 
     public override void Validate()
     {
