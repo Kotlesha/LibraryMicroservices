aa177ce182494ecf7b752c8603c9f746a98f1704
Kot Alexey <leshakot10@gmail.com>
2024-10-08 02:34:25 +0300

    Изменен файл DependencyInjection для внедрения автоматической валидации входных параметров сервисов

--- src/Services/User/Core/User.Application/DependencyInjection.cs
+++ src/Services/User/Core/User.Application/DependencyInjection.cs
@@ -1,5 +1,7 @@
 ﻿using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.CleanArchitecture.Application.Behaviours;
 using System.Reflection;
 
 namespace User.Application;
@@ -13,6 +15,7 @@ public static class DependencyInjection
         services.AddMediatR(configuration =>
             configuration.RegisterServicesFromAssemblies(assesmbly));
 
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationPipelineBehaviour<,>));
         services.AddValidatorsFromAssembly(assesmbly);
         services.AddAutoMapper(assesmbly);
 
