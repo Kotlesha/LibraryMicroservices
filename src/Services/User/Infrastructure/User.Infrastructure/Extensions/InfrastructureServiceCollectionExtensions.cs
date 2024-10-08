fead7d6a94d7ae634bb4d73a85b4da1d6b04a685
Kot Alexey <leshakot10@gmail.com>
2024-10-09 02:52:03 +0300

    Добавлена зависимость в DI-контайнер в виде конкретной реализации UoW

--- src/Services/User/Infrastructure/User.Infrastructure/Extensions/InfrastructureServiceCollectionExtensions.cs
+++ src/Services/User/Infrastructure/User.Infrastructure/Extensions/InfrastructureServiceCollectionExtensions.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.CleanArchitecture.Domain.Repositories;
 using User.Domain.Repositories;
 using User.Infrastructure.Context;
 using User.Infrastructure.Repositories;
+using Shared.CleanArchitecture.Infrastructure.Repositories;
 
 namespace User.Infrastructure.Extensions;
 
@@ -11,12 +13,12 @@ public static class InfrastructureServiceCollectionExtensions
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, 
         IConfiguration configuration)
     {
-        services.AddScoped<IUserRepository, UserRepository>();
-
         services.AddDbContext<UserDbContext>(options =>
             configuration.GetConnectionString("UserDbConnectionString"));
 
-        
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
         return services;
     }
 }
