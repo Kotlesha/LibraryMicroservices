2f60dd186846dff5854bf3db8501beb2a66b2a93
Kot Alexey <leshakot10@gmail.com>
2024-10-13 22:22:56 +0300

    Добавлен глобальный обработчик событий

---
+++ src/Shared/CleanArchitecture/Shared.CleanArchitecture.Presentation/Middleware/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shared.CleanArchitecture.Presentation.Middleware;
+
+public class GlobalExceptionHandlerMiddleware(
+    RequestDelegate next,
+    ILogger<GlobalExceptionHandlerMiddleware> logger)
+{
+    private readonly RequestDelegate _next = next;
+    private readonly ILogger<GlobalExceptionHandlerMiddleware> _logger = logger;
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, 
+                "Exception occurred: {Message}", ex.Message);
+
+            var problemDetails = new ProblemDetails
+            {
+                Status = StatusCodes.Status500InternalServerError,
+                Title = "Server Error"
+            };
+
+            context.Response.StatusCode =
+                StatusCodes.Status500InternalServerError;
+
+            await context.Response.WriteAsJsonAsync(problemDetails);
+        }
+    }
+}
