d34dda1f553afc5556bf79e40b840afde53adf88
Kot Alexey <leshakot10@gmail.com>
2024-12-04 17:48:33 +0300

    Реализован класс Error с использованием ErrorType

--- src/Shared/Shared.CleanArchitecture/Common/Components/Error.cs
+++ src/Shared/Shared.CleanArchitecture/Common/Components/Error.cs
@@ -1,12 +1,20 @@
 ﻿namespace Shared.CleanArchitecture.Common.Components;
 
-public class Error(string code, string message) : IEquatable<Error>
+public class Error : IEquatable<Error>
 {
-    public static readonly Error None = new(string.Empty, string.Empty);
-    public static readonly Error NullValue = new("Error.NullValue", "The specified result value is null.");
+    public static readonly Error None = new(string.Empty, string.Empty, ErrorType.Failure);
+    public static readonly Error NullValue = new("Error.NullValue", "The specified result value is null.", ErrorType.Failure);
 
-    public string Code { get; } = code;
-    public string Message { get; } = message;
+    public ErrorType Type { get; }
+    public string Code { get; }
+    public string Message { get; }
+
+    private Error(string code, string message, ErrorType type)
+    {
+        Code = code;
+        Message = message;
+        Type = type;
+    }
 
     public static implicit operator string(Error error) => error.Code;
 
@@ -42,4 +50,16 @@ public class Error(string code, string message) : IEquatable<Error>
     public override int GetHashCode() => HashCode.Combine(Code, Message);
 
     public override string ToString() => Code;
+
+    public static Error NotFound(string code, string message) =>
+        new(code, message, ErrorType.NotFound);
+
+    public static Error Validation(string code, string message) =>
+        new(code, message, ErrorType.Validation);
+
+    public static Error Conflict(string code, string message) =>
+        new(code, message, ErrorType.Conflict);
+
+    public static Error Failure(string code, string message) =>
+        new(code, message, ErrorType.Failure);
 }
