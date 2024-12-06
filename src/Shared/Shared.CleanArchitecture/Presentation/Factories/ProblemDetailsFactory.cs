e2d278ffaf13127751b7361ef3160fcb4d5ca896
Kot Alexey <leshakot10@gmail.com>
2024-12-06 16:50:55 +0300

    Введен клсс ProblemDetailsHelper

--- src/Shared/Shared.CleanArchitecture/Presentation/Factories/ProblemDetailsFactory.cs
+++ src/Shared/Shared.CleanArchitecture/Presentation/Factories/ProblemDetailsFactory.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Shared.CleanArchitecture.Common.Components.Errors;
+using Shared.CleanArchitecture.Presentation.Helpers;
 
 namespace Shared.CleanArchitecture.Presentation.Factories;
 
@@ -11,9 +11,9 @@ public static class ProblemDetailsFactory
     {
         var problemDetails = new ProblemDetails
         {
-            Status = GetStatusCode(error.Type),
-            Title = GetTitle(error.Type),
-            Type = GetType(error.Type),
+            Status = ProblemDetailsHelper.GetStatusCode(error.Type),
+            Title = ProblemDetailsHelper.GetTitle(error.Type),
+            Type = ProblemDetailsHelper.GetType(error.Type),
             Extensions = new Dictionary<string, object?>
             {
                 { "errors", new[] { error } }
@@ -22,34 +22,4 @@ public static class ProblemDetailsFactory
 
         return problemDetails;            
     }
-
-    private static int GetStatusCode(ErrorType errorType) =>
-        errorType switch
-        {
-            ErrorType.Validation => StatusCodes.Status422UnprocessableEntity,
-            ErrorType.NotFound => StatusCodes.Status404NotFound,
-            ErrorType.Conflict => StatusCodes.Status409Conflict,
-            ErrorType.BadRequest => StatusCodes.Status400BadRequest,
-            _ => StatusCodes.Status500InternalServerError
-        };
-
-    private static string GetTitle(ErrorType errorType) =>
-        errorType switch
-        {
-            ErrorType.Validation => "Unprocessable Entity Error",
-            ErrorType.NotFound => "Not Found Error",
-            ErrorType.Conflict => "Conflict Error",
-            ErrorType.BadRequest => "Bad Request Error",
-            _ => "Internal Server Error"
-        };
-
-    private static string GetType(ErrorType errorType) =>
-        errorType switch
-        {
-            ErrorType.Validation => "https://datatracker.ietf.org/doc/html/rfc4918#section-11.2",
-            ErrorType.BadRequest => "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.1",
-            ErrorType.NotFound => "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.4",
-            ErrorType.Conflict => "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.8",
-            _ => "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1"
-        };
 }
