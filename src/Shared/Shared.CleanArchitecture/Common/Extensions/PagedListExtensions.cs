50d9b50a4132af54be9c7dd6026b3ce4d5c95549
Kot Alexey <leshakot10@gmail.com>
2024-12-08 23:43:43 +0300

    Добавлены базовые классы, необходимые для реализации пагинации

---
+++ src/Shared/Shared.CleanArchitecture/Common/Extensions/PagedListExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.CleanArchitecture.Common.Paging;
+
+namespace Shared.CleanArchitecture.Common.Extensions;
+
+public static class PagedListExtensions
+{
+    public static async Task<PagedList<T>> ToPagedList<T>(
+        this IQueryable<T> query, int pageNumber, int pageSize)
+    {
+        var count = await query.CountAsync();
+
+        var items = await query
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+
+        return new PagedList<T>(items, count, pageNumber, pageSize);
+    }
+}
