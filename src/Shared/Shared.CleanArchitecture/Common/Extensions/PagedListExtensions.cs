893ffa1ec7f21fb1a412b263ce3c8a3f7ea2a9da
Kot Alexey <leshakot10@gmail.com>
2024-12-09 00:27:02 +0300

    Добавлена поддержка CancellationToken в ToPagedList

--- src/Shared/Shared.CleanArchitecture/Common/Extensions/PagedListExtensions.cs
+++ src/Shared/Shared.CleanArchitecture/Common/Extensions/PagedListExtensions.cs
@@ -6,14 +6,17 @@ namespace Shared.CleanArchitecture.Common.Extensions;
 public static class PagedListExtensions
 {
     public static async Task<PagedList<T>> ToPagedList<T>(
-        this IQueryable<T> query, int pageNumber, int pageSize)
+        this IQueryable<T> query, 
+        int pageNumber, 
+        int pageSize, 
+        CancellationToken cancellationToken)
     {
-        var count = await query.CountAsync();
+        var count = await query.CountAsync(cancellationToken);
 
         var items = await query
             .Skip((pageNumber - 1) * pageSize)
             .Take(pageSize)
-            .ToListAsync();
+            .ToListAsync(cancellationToken);
 
         return new PagedList<T>(items, count, pageNumber, pageSize);
     }
