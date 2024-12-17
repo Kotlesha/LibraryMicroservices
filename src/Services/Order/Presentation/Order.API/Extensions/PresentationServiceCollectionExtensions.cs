952bbcdce283ef1dae666034370eebd557739c09
Alesya <ananko_aa_21@mf.grsu.by>
2024-12-17 20:49:03 +0300

    Создала класс OrderEndpoints с методами CreateOrder, DeleteOrder, UpdateOrder, GetOrderByUserId, GetOrderByCreatedTime. Создала Extensions и изменила Program.cs

---
+++ src/Services/Order/Presentation/Order.API/Extensions/PresentationServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Shared.CleanArchitecture.Application.Abstractions.Providers;
+using Shared.CleanArchitecture.Presentation.Providers;
+using Shared.Components.ExceptionHandling.Extensions;
+
+namespace Order.API.Extensions;
+
+public static class PresentationServiceCollectionExtensions
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        services.AddHttpContextAccessor();
+        services.AddScoped<IUserIdProvider, UserIdProvider>();
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddExtendedProblemDetails();
+
+        return services;
+    }
+}
--- src/Services/Order/Presentation/Order.API/Program.cs
+++ src/Services/Order/Presentation/Order.API/Program.cs
@@ -1,36 +1,47 @@
+using Shared.Components.ExceptionHandling.Middleware;
+using Shared.Components.Migrations;
+using Order.API.Endpoints;
+using Order.API.Extensions;
+using Order.Application.Extensions;
+using Order.Infrastructure.Context;
+using Order.Infrastructure.Extensions;
 
-namespace Order.API
+namespace Order.API;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddApplication();
+        builder.Services.AddInfrastructure(builder.Configuration);
+        builder.Services.AddPresentation();
+
+        builder.Services.AddAuthorization();
+        builder.Services.AddAuthentication();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.ApplyMigrations<OrderDbContext>();
         }
+
+        app.MapOrderEndpoints();
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseStatusCodePages();
+
+        app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
+
+        app.Run();
     }
 }
+
